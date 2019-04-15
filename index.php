77b25ec4c6e70b41f30f9720f273729acb84c459
Evan Wheeler <effyn@gmx.com>
2019-04-15 16:16:28 -0400

    Add view

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->set('DEBUG', 3);
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
